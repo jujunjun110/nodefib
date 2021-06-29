94ce5f36b0f8b0d5c00fb63d31e3c60ebe92cf09
Jun Ito <jujunjun110@gmail.com>
2021-06-29 21:55:36 +0900

    Update: 末尾再帰の書き方に変更

--- app/fib.ts
+++ app/fib.ts
@@ -1,7 +1,19 @@
 export const fib = function (num: number): number {
-	if (num == 1 || num == 2) {
-		return 1
+	return fib_inner(num, 0, 1)
+}
+
+const fib_inner = function (num: number, a: number, b: number): number {
+	if (num < 0) {
+		return -fib_inner(-num, 0, 1)
 	}
 
-	return fib(num - 1) + fib(num - 2)
+	if (num == 0) {
+		return 0
+	}
+
+	if (num == 1) {
+		return b
+	}
+
+	return fib_inner(num - 1, b, a + b)
 }
--- test/fib_test.ts
+++ test/fib_test.ts
@@ -3,6 +3,8 @@ import { fib } from "../app/fib";
 
 it("return fib number", () => {
 	const cases = [
+		[-10, -55],
+		[0, 0],
 		[1, 1],
 		[2, 1],
 		[10, 55]
